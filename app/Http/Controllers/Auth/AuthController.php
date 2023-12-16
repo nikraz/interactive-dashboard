3597e5e29ab79ac0ce3beebfddc0489376ae0bdc
nikolay.razlozhki <nikolay.razlozhki@mentormate.com>
2023-12-16 15:17:21 +0200

    Added login controller

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('API Token')->plainTextToken;
+
+            return response()->json(['message' => 'Login successful', 'token' => $token], 200);
+        }
+
+        return response()->json(['error' => 'The provided credentials do not match our records.'], 401);
+    }
+}
